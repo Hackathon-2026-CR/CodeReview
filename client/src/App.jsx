1fb975b5e4094209263f383b3fe202208184a589
aviel <avieloikpro@gmail.com>
2026-03-23 16:57:38 +0200

    app.js Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AllTasksPage from './pages/AllTasksPage'
+import MyTasksPage from './pages/MyTasksPage'
+import AddTaskPage from './pages/AddTaskPage'
+import TaskDetailsPage from './pages/TaskDetailsPage'
 
 function App() {
 
   return (
     <div>
-      My-App
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/all-tasks" element={<AllTasksPage />} />
+          <Route path="/my-tasks" element={<MyTasksPage />} />
+          <Route path="/add-task" element={<AddTaskPage />} />
+          <Route path="/task-details/:id" element={<TaskDetailsPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
