b73e143315743a7b0889d992f36b1806b60f9b43
shalom berman 7 <bermanshalom7@gmail.com>
2026-03-23 17:03:05 +0200

    add: base endpoints

--- hacaton/querys/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter(
-    prefix="/api/tasks",
-    tags=["tasks"]
-)
-
---
+++ querys/router.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+from querys import dal
+from querys.moduls import TaskCreate
+
+
+router = APIRouter(
+    prefix="/api/tasks",
+    tags=["tasks"]
+)
+
+
+@router.get('/users/{user_name}')
+def get_user_info(user_name):
+    pass
+
+
+@router.get('/my-tasks/{user_name}')
+def get_tasks_created_by_user(user_name):
+    pass
+
+
+@router.get('/working-tasks/{user_name}')
+def get_tasks_the_user_is_currently_working_on(user_name):
+    pass
+
+
+@router.post('/add-task')
+def add_task(task: TaskCreate):
+    pass
+
+
+@router.get('/all-tasks')
+def get_all_tasks():
+    pass
+
+
+@router.get('/{id}')
+def get_task_using_id(id):
+    pass
